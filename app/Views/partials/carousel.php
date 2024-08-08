c2b2527f1f18635d76fc7751803fb89c1afb8955
shubhth <godfreymatagaro@gmail.com>
2024-08-08 09:42:28 +0300

    hero ux done done

--- app/Views/partials/carousel.php
+++ app/Views/partials/carousel.php
@@ -1,1 +1,23 @@
-<section class="carousel"><button class="prev-btn"><i class="material-icons">keyboard_arrow_left</i></button><div class="carousel-container"><div class="carousel-item"><img src="<?= base_url('/public/images/nyeri_hse.jpg') ?>" alt="Image 1"></div><div class="carousel-item"><img src="<?= base_url('/public/images/muthaiga2.jpg') ?>" alt="Image 2"></div><div class="carousel-item"><img src="<?= base_url('/public/images/mm4.jpg') ?>" alt="Image 2"></div></div><button class="next-btn"><i class="material-icons">keyboard_arrow_right</i></button><div class="carousel-dots"></div></section>
+<section class="carousel">
+  <button class="prev-btn">
+    <i class="material-icons">keyboard_arrow_left</i>
+  </button>
+  <div class="carousel-container">
+    <div class="carousel-item">
+      <img src="
+				<?= base_url('/public/images/nyeri_hse.jpg') ?>" alt="Image 1">
+    </div>
+    <div class="carousel-item">
+      <img src="
+					<?= base_url('/public/images/muthaiga2.jpg') ?>" alt="Image 2">
+    </div>
+    <div class="carousel-item">
+      <img src="
+						<?= base_url('/public/images/mm4.jpg') ?>" alt="Image 2">
+    </div>
+  </div>
+  <button class="next-btn">
+    <i class="material-icons">keyboard_arrow_right</i>
+  </button>
+  <div class="carousel-dots"></div>
+</section>
